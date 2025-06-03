8054353c46d89ba9cd2dc9d70a4674d89a89ed8c
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-06-03 23:48:12 +0200

    refactor: improve robots.txt logic for non-production environments

--- robots.ts
+++ robots.ts
@@ -1,6 +1,17 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  if (process.env.nodeEnv !== 'production') {
+    return {
+      rules: [
+        {
+          disallow: '/',
+          userAgent: '*',
+        },
+      ],
+    };
+  }
+
   return {
     rules: {
       allow: '/',
